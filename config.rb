15b97bdd44cdda469ee52aabdf0f48ee8fabf03f
alfe <alfe10251@gmail.com>
2012-09-17 09:37:04 +0900

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,8 +1,8 @@
 ﻿#coding: utf-8
 
-@username='alfe10251@gmail.com' #gmailのアドレス
-@password='alfe6204' #gmailのパスワード
+@username='***@gmail.com' #gmailのアドレス
+@password='***' #gmailのパスワード
 @mail_label = 'Work/maid-tyan'
 
-@address ="b.a-t-m.100609@docomo.ne.jp" #送信先アドレス
+@address ="******@docomo.ne.jp" #送信先アドレス
 @zipcode = "572-0077" #郵便番号
