1442c70a4f55e2da9d4537e7943d0ba3a60d9f5b
alfe <alfe10251@gmail.com>
2012-11-09 07:05:43 +0900

    Update config.rb

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 
 @username='hogehoge@gmail.com' #gmailのアドレス
 @password='***' #gmailのパスワード
-@mail_label = 'Work/maid-tyan'
+@mail_label = 'Work/maid-tyan' #メイドちゃん宛のメールを事前に振り分けてください。
 
 @address ="******@docomo.ne.jp" #送信先アドレス
 @zipcode = "572-0077" #郵便番号
